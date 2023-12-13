5b931459cfa31bca0ecd8afba15e9cab5249d2b1
Hal Carleton <>
2023-12-13 15:26:53 -0800

    chore: integrate radix-ui theme into landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import { Theme } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body className={inter.className}>
+        <Theme>
+          {children}
+        </Theme>
+      </body>
     </html>
   )
 }
