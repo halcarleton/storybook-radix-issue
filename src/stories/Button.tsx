3f7a495700b6c9baa519952762b7ee2bd9f05730
Hal Carleton <>
2023-12-13 15:28:14 -0800

    feature: update Button component to use Radix theme

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import './button.css';
+import { Button as RadixButton } from '@radix-ui/themes'
 
 interface ButtonProps {
   /**
    * Is this the principal call to action on the page?
    */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
   /**
    * How large should the button be?
    */
@@ -30,23 +26,15 @@ interface ButtonProps {
 export const Button = ({
   primary = false,
   size = 'medium',
-  backgroundColor,
   label,
-  ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const variant = primary ? 'solid' : 'outline';
+  const sizeMap = {small: '1', medium: '2', large: '3'};
+  const sizeNumber = sizeMap[size] as ('1' | '2' | '3');
+
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      {...props}
-    >
+    <RadixButton variant={variant} size={sizeNumber}>
       {label}
-      <style jsx>{`
-        button {
-          background-color: ${backgroundColor};
-        }
-      `}</style>
-    </button>
+    </RadixButton>
   );
 };
