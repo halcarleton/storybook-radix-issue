3f2e4ff69224f0f5a48e1b44a46d9f72a138d669
Hal Carleton <>
2023-12-13 15:06:05 -0800

    chore: integrate radix-ui theme into storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,12 @@
+import React from 'react';
 import type { Preview } from "@storybook/react";
+import { Theme } from "@radix-ui/themes";
+import '@radix-ui/themes/styles.css';
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (<Theme><Story /></Theme>),
+  ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
